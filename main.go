0bdf0d63925e6233bc72c5dfa8c924f774b42da0
axit <axit@Axit-Dhola.local>
2023-01-19 12:46:48 +0530

    Modify Update and Delete

--- main.go
+++ main.go
@@ -2,122 +2,121 @@ package main
 
 import (
 	"database/sql"
-	"net/http"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
 )
 
-type Person struct{
-	ID    int `json:"id"`
-	Name  string `json:"name"`
-} 
+type Person struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
-func resultQuery(query string) ([]Person , bool){
+func resultQuery(query string) ([]Person, bool) {
 
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/Config")
 	if err != nil {
 		panic(err.Error())
-		return nil , false
+		return nil, false
 	}
 	defer db.Close()
 
 	queryResult, err := db.Query(query)
-    if err != nil {
+	if err != nil {
 		panic(err.Error())
-		return nil , false
+		return nil, false
 	}
 	defer queryResult.Close()
-    
+
 	var result []Person
 	for queryResult.Next() {
-        var newPerson Person
-        err = queryResult.Scan(&newPerson.ID,&newPerson.Name)
-        if err != nil {
+		var newPerson Person
+		err = queryResult.Scan(&newPerson.ID, &newPerson.Name)
+		if err != nil {
 			panic(err.Error())
-			return nil , false
+			return nil, false
 		}
-        result = append(result,newPerson)
-    }
-	return result , true 
+		result = append(result, newPerson)
+	}
+	return result, true
 }
 
-func getPerson(context *gin.Context){  // Context contains data
-	   
-	result , flag := resultQuery("SELECT * FROM demo1")
-	if(!flag) {
+func getPerson(context *gin.Context) { // Context contains data
+
+	result, flag := resultQuery("SELECT * FROM demo1")
+	if !flag {
 		return
 	}
-    context.IndentedJSON(http.StatusOK , result)
+	context.IndentedJSON(http.StatusOK, result)
 }
 
-func addPerson(context *gin.Context){
+func addPerson(context *gin.Context) {
 	var newPerson Person
 	err := context.BindJSON(&newPerson)
-	if(err != nil) {
+	if err != nil {
 		return
 	}
-	
+
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/Config")
 	if err != nil {
 		return
 	}
 	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO demo1 VALUES (?,?)" , newPerson.ID , newPerson.Name)
+	insert, err := db.Query("INSERT INTO demo1 VALUES (?,?)", newPerson.ID, newPerson.Name)
 	defer insert.Close()
-	context.IndentedJSON(http.StatusCreated , newPerson)  // (status , JSON)
+	context.IndentedJSON(http.StatusCreated, newPerson) // (status , JSON)
 }
 
-func deletePerson(context *gin.Context){
+func deletePerson(context *gin.Context) {
 	id := context.Param("id")
-    db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/Config")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/Config")
 	if err != nil {
 		panic(err.Error())
-	    return
+		return
 	}
 	defer db.Close()
 
-	res, err:= db.Query("DELETE FROM demo1 WHERE pid = ?" , id)
+	res, err := db.Query("DELETE FROM demo1 WHERE pid = ?", id)
 	defer res.Close()
 	if err != nil {
 		return
 	}
-	
-	context.IndentedJSON(http.StatusOK, "completed")
+
+	context.IndentedJSON(http.StatusOK, "Deleted")
 }
 
-func updatePerson(context *gin.Context){
+func updatePerson(context *gin.Context) {
 	var newPerson Person
 	err := context.BindJSON(&newPerson)
-	if(err != nil) {
+	if err != nil {
 		return
 	}
 
 	id := context.Param("id")
-    db, err := sql.Open("mysql" , "root:root@tcp(localhost:3306)/Config")
-	if(err!=nil){
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/Config")
+	if err != nil {
 		panic(err.Error())
 		return
 	}
 
 	defer db.Close()
 
-	res, err := db.Query("UPDATE demo1 SET Name = ? WHERE pid = ?" , newPerson.Name , id)
+	res, err := db.Query("UPDATE demo1 SET Name = ? WHERE pid = ?", newPerson.Name, id)
 	defer res.Close()
-    if(err!=nil){
+	if err != nil {
 		panic(err.Error())
 		return
 	}
-    
-	context.IndentedJSON(http.StatusOK, res)
+
+	context.IndentedJSON(http.StatusOK, "Updated")
 }
 
-func main(){
-	router := gin.Default() // create server 
-	router.GET("/Person" , getPerson)
-	router.POST("/Person" , addPerson)
-	router.PUT("/Person/:id" , updatePerson)
-	router.DELETE("/Person/:id" , deletePerson) // : represent dynamic
-	router.Run("localhost:9090") // path endpoint
+func main() {
+	router := gin.Default() // create server
+	router.GET("/Person", getPerson)
+	router.POST("/Person", addPerson)
+	router.PUT("/Person/:id", updatePerson)
+	router.DELETE("/Person/:id", deletePerson) // : represent dynamic
+	router.Run("localhost:9090")               // path endpoint
 }
-
